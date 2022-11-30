bb4d89efb79bf81a22144e2bb79eb60a2c01d5e8
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2022-11-30 18:57:59 +0100

    add parameters

--- software/src/main/java/com/claranet/vies/proxy/model/ValidationRequest.java
+++ software/src/main/java/com/claranet/vies/proxy/model/ValidationRequest.java
@@ -1,15 +1,13 @@
 package com.claranet.vies.proxy.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.annotation.JsonCreator;
 
 public class ValidationRequest {
     private final String country;
     private final String vatNumber;
 
     @JsonCreator
-    public ValidationRequest(@JsonProperty("country") String country,
-                             @JsonProperty("vatNumber") String vatNumber) {
+    public ValidationRequest(String country, String vatNumber) {
         this.country = country;
         this.vatNumber = vatNumber;
     }
