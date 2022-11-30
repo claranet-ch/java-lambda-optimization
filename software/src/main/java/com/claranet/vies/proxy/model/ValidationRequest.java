27bb270482707e0584e3f6353014f9045733cc05
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2022-11-30 19:08:52 +0100

    add explicit parameters annotation

--- software/src/main/java/com/claranet/vies/proxy/model/ValidationRequest.java
+++ software/src/main/java/com/claranet/vies/proxy/model/ValidationRequest.java
@@ -1,13 +1,15 @@
 package com.claranet.vies.proxy.model;
 
 import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.annotation.JsonCreator;
+import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ValidationRequest {
     private final String country;
     private final String vatNumber;
 
     @JsonCreator
-    public ValidationRequest(String country, String vatNumber) {
+    public ValidationRequest(@JsonProperty("country") String country,
+                             @JsonProperty("vatNumber") String vatNumber) {
         this.country = country;
         this.vatNumber = vatNumber;
     }
